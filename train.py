5bf83c419ac269ef6e98fbb40aec54494ca02e1a
Ibrahim Ali <ibrahim.hassan01@eng-st.cu.edu.eg>
2024-03-01 14:50:19 +0200

    adding train_model function

--- train.py
+++ train.py
@@ -73,4 +73,67 @@ def get_model(config, source_vocab_size, target_vocab_size, ):
         config['dropout']
     )
 
+def train_model(config):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'using device {device}')
+
+    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+
+    train_loader = get_dataset('iwslt2017', config)['train_dataloader']
+    valid_loader = get_dataset('iwslt2017', config)['valid_dataloader']
+    source_tokenizer = get_dataset('iwslt2017', config)['source_tokenizer']
+    target_tokenizer = get_dataset('iwslt2017', config)['target_tokenizer']
+
+    model = get_model(config, source_tokenizer.get_vocab_size(), target_tokenizer.get_vocab_size()).to(device)
+
+    writer = SummaryWriter(config['experiment_name'])
+
+    optimizer = Adam(model.parameters(), lr=config['learning_rate'], eps=1e-9)
+
+    initial_epoch = 0
+    global_step = 0
+
+    if config['preload']:
+        model_filename = get_weights_file_path(config, config['preload'])
+        print(f'preloading model {model_filename}')
+        state = torch.load(model_filename)
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+
+    criterion = nn.CrossEntropyLoss(ignore_index=source_tokenizer.token_to_id('<PAD>'), label_smoothing=0.1).to(device)
+    epochs = config['num_epochs']
+
+    for epoch in range(initial_epoch, epochs):
+        model.train()
+        batch_iterator = tqdm(train_loader, desc=f'processing epoch {epoch:02d}')
+        for batch in batch_iterator:
+            encoder_input = batch['encoder_input'].to(device) #! (B, max_len)
+            decoder_input = batch['decoder_input'].to(device) #! (B, max_len)
+            encoder_mask = batch['encoder_mask'].to(device) #! (B, 1, 1, max_len)
+            decoder_mask = batch['decoder_mask'].to(device) #! (B, 1, max_len, max_len)
+            label = batch['label'].to(device) #! (B, max_len)
+
+            encoder_output = model.encode(encoder_input, encoder_mask) #! (B, max_len, d_model)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) #! (B, max_len, d_model)
+            proj_output = model.project(decoder_output) #! (B, max_len, target_vocab_size)
+
+            loss = criterion(proj_output.view(-1, target_tokenizer.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({'loss': f'{loss.item():6.3f}'})
+
+            writer.add_scaler('train_loss', loss.item, global_step)
+            writer.flush()
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            global_step += 1
+
+        model_filename = get_weights_file_path(config, epoch)
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'global_step': global_step,
+        }, model_filename)
 
